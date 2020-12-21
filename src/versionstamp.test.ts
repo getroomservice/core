49ad7f020b53430e0b1aff08e3bba7232f8fc12e
Kevin King <4kevinking@gmail.com>
2020-12-21 13:41:16 -0800

    add tests with different lengths

--- src/versionstamp.test.ts
+++ src/versionstamp.test.ts
@@ -18,8 +18,20 @@ test('compareVS(older, newer) === true, 2', () => {
 
 const zero: string = 'AAAAAAAAAAAAAAo=';
 const one: string = 'AAAAAAAAAAAAAQo=';
+const zeroZero = 'AAAK';
+const zeroTwo = 'AAIK';
 
 test('0 < 1', () => {
   expect(vs.isOlderVS(zero, one)).toEqual(true);
   expect(vs.isOlderVS(one, zero)).toEqual(false);
+});
+
+test('00 < 1', () => {
+  expect(vs.isOlderVS(zeroZero, one)).toEqual(true);
+  expect(vs.isOlderVS(one, zeroZero)).toEqual(false);
+});
+
+test('02 > 1', () => {
+  expect(vs.isOlderVS(one, zeroTwo)).toEqual(true);
+  expect(vs.isOlderVS(zeroTwo, one)).toEqual(false);
 });
