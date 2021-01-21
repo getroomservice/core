a800d11ce1d7eb1c11edf9841523226cf3f5c0bf
Kevin King <4kevinking@gmail.com>
2021-01-21 13:15:39 -0800

    add react native b64Decode impl

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,18 @@
 export const b64Decode = (function(): (s: string) => string {
+  if (isWeb()) {
+    return window.atob;
+  }
   if (isNodejs()) {
     return s => Buffer.from(s, 'base64').toString('binary');
+  }
+  if (isReactNative()) {
+    const base64 = require('base64-js');
+    //    note this only works for short sequences because it is recursive
+    return (s: string) =>
+      String.fromCharCode.apply(
+        null,
+        Array.from((base64.toByteArray(s) as Uint8Array).values())
+      );
   }
   return window.atob;
 })();
@@ -12,6 +24,7 @@ export const generateID: () => string = (() => {
       return crypto.randomBytes(16).toString('base64');
     };
   } else {
+    //  note React Native also takes this branch, but needs "react-native-get-random-values" npm package installed
     return () => {
       const arr = new Uint8Array(16);
       crypto.getRandomValues(arr);
@@ -21,11 +34,21 @@ export const generateID: () => string = (() => {
   }
 })();
 
-function isNodejs() {
+function isWeb(): boolean {
+  return typeof document !== 'undefined';
+}
+
+function isNodejs(): boolean {
   return (
     typeof 'process' !== 'undefined' &&
-    process &&
-    process.versions &&
-    process.versions.node
+    !!process &&
+    !!process.versions &&
+    !!process.versions.node
+  );
+}
+
+function isReactNative(): boolean {
+  return (
+    typeof navigator !== 'undefined' && navigator.product === 'ReactNative'
   );
 }
